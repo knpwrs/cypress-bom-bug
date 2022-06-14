0f636cc12179273f1ffd451d899be29e7bc56839
Ken Powers <ken@kenpowers.net>
2022-06-14 16:26:18 -0500

    Add failing spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
 /// <reference types="cypress" />
-describe('page', () => {
-  it('works', () => {
-    cy.visit('https://example.cypress.io')
+describe('bom', () => {
+  it('contains cannot find text with bom', () => {
+    cy.visit('http://localhost:5657')
+    cy.contains('\ufefftesting\ufeff\ufeff and stuff')
+  })
+
+  it('we can still assert against text with bom', () => {
+    cy.visit('http://localhost:5657')
+    cy.get('#target')
+      .should((div) => {
+        const text = div.text()
+        // cy.contains() does not appear to be able to find elements with \ufeff, so we assert manually
+        expect(text).to.eq('\ufefftesting\ufeff\ufeff and stuff')
+      })
+
   })
 })
